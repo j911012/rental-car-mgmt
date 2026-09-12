486064733b5edeea2adaa1ef2a23869297ec4a08
jun <juntetsu.1012@gmail.com>
2026-09-12 23:31:47 +0900

    Spring Bootプロジェクト雛形を作成

---
+++ src/main/java/com/example/rental/RentalCarMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.example.rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalCarMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalCarMgmtApplication.class, args);
+	}
+
+}
